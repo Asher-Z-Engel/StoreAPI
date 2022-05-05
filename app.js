6f62e4af190e1dc5775e3aa25843cff49dac8f72
Asher <asherzengel@gmail.com>
2022-05-05 20:37:11 +0300

    basic error handling

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const connectDB = require('./db/connect')
 // routes
 const products = require('./routes/products')
+// errors
+const notFound = require('./errors/NotFound')
+const errorHandler = require('./errors/errorHandler')
 
 app.use('/api/v1/products', products)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const port = process.env.PORT || 5000
 const start = async () => {
